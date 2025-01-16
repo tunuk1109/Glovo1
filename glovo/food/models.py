f7c0af6abbebba43cb8ed133ad0ece543605a649
Asanbekova <TunukAsanbekova>
2025-01-17 01:01:53 +0600

    I filled the folder models.py

--- glovo/food/models.py
+++ glovo/food/models.py
@@ -1,3 +1,140 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db.models import DecimalField
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class UserProfile(AbstractUser):
+    age = models.PositiveSmallIntegerField(validators=[MinValueValidator(18), MaxValueValidator(75)],
+                                           null=True, blank=True)
+    phone_number = PhoneNumberField(null=True, blank=True)
+    STATUS_CHOICES = (
+        ('client', 'client'),
+        ('courier', 'courier'),
+        ('owner', 'owner'),
+    )
+    status = models.CharField()
+    user_image = models.ImageField(choices=STATUS_CHOICES, max_length=32, default='client')
+
+    def __str__(self):
+        return f'{self.first_name}, {self.last_name}'
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=32, unique=True)
+
+    def __str__(self):
+        return self.category_name
+
+
+class Store(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    owner = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    store_name = models.CharField(max_length=32, unique=True)
+    description = models.TextField()
+    address = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.store_name
+
+
+class ContactInfo(models.Model):
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    title = models.CharField(max_length=32)
+    phone_numbers = PhoneNumberField()
+    social_network = models.CharField(max_length=32, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.store}'
+
+
+class Cart(models.Model):
+    user = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user}'
+
+
+class Product(models.Model):
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product_name = models.CharField(max_length=64)
+    product_image = models.ImageField()
+    price = models.DecimalField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.product_name
+
+
+class CarItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField(default=1)
+
+
+class ProductCombo(models.Model):
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    combo_name = models.CharField(max_length=32)
+    combo_image = models.ImageField()
+    combo_price = DecimalField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.combo_name
+
+
+class Burgers(models.Model):
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    burgers_image = models.ImageField()
+
+
+class Order(models.Model):
+    client = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    courier = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    STATUS_ORDER_CHOICES = (
+        ('waiting processing', 'waiting processing'),
+        ('process of delivery', 'process of delivery'),
+        ('delivered', 'delivered'),
+        ('cancelled', 'cancelled')
+    )
+    order_status = models.CharField(choices=STATUS_ORDER_CHOICES, max_length=16)
+    product_combo = models.ForeignKey(ProductCombo, on_delete=models.CASCADE)
+    delivery_address = models.CharField(max_length=32)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+
+class Courier(models.Model):
+    courier = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    STATUS_CHOICES = (
+        ('available', 'available'),
+        ('busy', 'busy')
+    )
+    status_courier = models.CharField(choices=STATUS_CHOICES, max_length=16, default='available')
+    current_order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+
+class ReviewStore(models.Model):
+    client = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
+    comment = models.TextField()
+
+
+class RatingCourier(models.Model):
+    courier = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    stars = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
+
+
+
+
+
+
+
+
+
+
+
